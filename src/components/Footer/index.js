050f34490aff91ea5163abafd151ccddb0c23884
tocteman <juanfrancisco.vinueza@gmail.com>
2018-05-18 18:27:22 +0200

    nuevo footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React from "react"
+import Link from "gatsby-link"
 import FaTwitter from 'react-icons/lib/fa/twitter'
 import FaFacebookOfficial from 'react-icons/lib/fa/facebook-official'
 import FaInstagram from 'react-icons/lib/fa/instagram'
@@ -8,21 +9,50 @@ import FintechWinner from '../../img/footer/fintech.jpg'
 
 
 const Footer = () => (
-  <div className="bg-grey-darkest">
-    <div className="text-center sm:text-left sm:flex justify-around items-center max-w-xl mx-auto p-4 md:p-4">
-      <div className="md:w-1/3 mx-auto text-center">
-        <p className="text-white font-medium no-underline text-grey-lightest leading-normal">© 2016 - 2017 Übank App <br/>
-        <span className="text-grey-light">Millennial Fintech Services LLC.</span> <br/>
-        <span className="text-grey-dark">334 Blackwell St, Durham, NC 27701</span> </p>
-    </div>
-      <div className="flex justify-center items-center text-white w-1/3 text-center mx-auto text-center my-8 md:my-2">
-        <div className="m-4 text-xl cursor-pointer hover:text-grey-lighter"><FaTwitter /></div>
-        <div className="m-4 text-xl cursor-pointer hover:text-grey-lighter"><FaFacebookOfficial /></div>
-        <div className="m-4 text-xl cursor-pointer hover:text-grey-lighter"><FaInstagram /></div>
-        <div className="m-4 text-xl cursor-pointer hover:text-grey-lighter"><FaYoutubePlay /></div>
+  <div className="bg-grey-darkest text-grey-lightest py-4 leading-normal">
+    <div className="text-center md:text-left md:flex justify-between items-start max-w-xl mx-auto p-4 md:p-4">
+      <div className="sm:w-1/2 lg:w-1/4 my-8 md:my-2 mx-auto text-center md:text-left text-grey-lightest">
+        <p className="font-medium text-white pb-2 text-xl">Übank</p>
+        <ul className="list-reset font-light leading-normal">
+          <li><Link to="/equipo/" className="text-grey-lightest no-underline hover:text-white hover:border-white hover:border-b-2">Acerca de nosotros</Link></li>
+          <li><Link to="/unete-equipo/" className="text-grey-lightest no-underline hover:text-white hover:border-white hover:border-b-2"> Trabaja con nosotros</Link></li>
+          <li>Blog</li>
+          <li>Preguntas Frecuentes</li>
+          <li>Precio</li>
+        </ul>
       </div>
-      <div className="w-1/3 my-8 md:my-2 mx-auto text-center">
-        <img src={FintechWinner} width="150"/>
+
+      <div className="sm:w-1/2 lg:w-1/4 my-8 md:my-2 mx-auto text-center md:text-left text-grey-lightest">
+        <p className="font-medium text-white mb-2 text-xl">Contacto</p>
+        <p className="font-light">hola[at]ubankapp.com <br/>
+        +555281070021</p>
+        <p className="font-light pt-4"><span className="font-medium text-white">Estados Unidos</span><br/>
+        334 Blackwell Street<br/>
+        Durham, NC</p>
+        <p className="font-light pt-4"><span className="font-medium text-white">México</span><br/>
+        Chihuahua 230, Roma Norte<br/>
+        CDMX, DF</p>
+        <p className="font-light pt-4"><span className="font-medium text-white">Chile</span><br/>
+        Carlos Antúnez 1843, Oficina 607<br/>
+        Providencia, Santiago</p>
+      </div>
+
+      <div className="sm:w-1/2 lg:w-1/4 my-8 md:my-2 mx-auto text-center md:text-left text-grey-lightest">
+      <p className="font-medium text-white text-xl pb-2">Legal</p>
+      <ul className="list-reset font-light">
+        <li>Aviso de Privacidad</li>
+        <li>Términos y condiciones</li>
+        <li>Seguridad</li>
+      </ul>
+      </div>
+      
+      <div className="text-grey-lightest sm:w-1/2 lg:w-1/4 text-center mx-auto text-center my-8 md:my-2">Descubre nuevas formas de ahorrar en nuestras redes sociales.
+          <div className="flex justify-center items-center mx-auto text-center">
+          <div className="m-4 text-xl cursor-pointer hover:text-grey-lighter"><a href="https://twitter.com/ubankapp" target="_blank" className="no-underline text-white hover:text-grey-lighter hover:border-white hover:border-b-2"><FaTwitter /></a></div>
+          <div className="m-4 text-xl cursor-pointer hover:text-grey-lighter"><a href="https://facebook.com/ubanklatam" target="_blank" className="no-underline text-white hover:text-grey-lighter hover:border-white hover:border-b-2"><FaFacebookOfficial /></a></div>
+          <div className="m-4 text-xl cursor-pointer hover:text-grey-lighter"><a href="https://instagram.com/ubank_app/" target="_blank" className="no-underline text-white hover:text-grey-lighter hover:border-white hover:border-b-2"><FaInstagram /></a></div>
+            <div className="m-4 text-xl cursor-pointer hover:text-grey-lighter"><FaYoutubePlay /></div>
+          </div>
       </div>
     </div>
   </div>
